15b6a3861eab0197404871798f5cc1d1babc2641
isabelizimm <isabel.zimmerman@rstudio.com>
2022-02-08 17:28:30 -0500

    fastapi subclass and predict method

--- vetiver/server.py
+++ vetiver/server.py
@@ -1,90 +1,118 @@
-from logging import warn
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-from pydantic import BaseModel, ValidationError
-import joblib
-from joblib import dump
+from fastapi.responses import HTMLResponse, RedirectResponse
 import uvicorn
+import joblib
 from typing import List, Optional
-import nest_asyncio
-import warnings
+from logging import warn
+import requests
 import numpy as np
 import pandas as pd
 
 from vetiver.vetiver_model import VetiverModel
+from vetiver.utils import _jupyter_nb
 
-class VetiverApp():
-    
+
+class VetiverServe(FastAPI):
+
     app = None
 
     def __init__(
-        self, 
-        model: VetiverModel, 
-        check_ptype: bool = True, 
-        port: Optional[int]=8000, 
-        host = "127.0.0.1"
+        self,
+        model: VetiverModel,
+        check_ptype: bool = True,
+        port: Optional[int] = 8000,
+        host="127.0.0.1",
     ) -> None:
-        self.app = _init_app(model, check_ptype)
+        self.model = model
         self.port = port
         self.host = host
+        self.check_ptype = check_ptype
+        self.app = self._init_app()
+
+    def _init_app(self):
+
+        ptype = self.model.ptype[0]
+        served_model = _prepare_model(self.model.model[0])
+
+        app = FastAPI()
+
+        @app.get("/", response_class=HTMLResponse, include_in_schema=False)
+        def docs_redirect():
+            return RedirectResponse("/rapidoc")
+
+        @app.get("/ping", include_in_schema=False)
+        def ping():
+            return {"ping": "pong"}
+
+        @app.get("/rapidoc", response_class=HTMLResponse, include_in_schema=False)
+        async def rapidoc():
+            return f"""
+                <!doctype html>
+                <html>
+                    <head>
+                        <meta charset="utf-8">
+                        <script
+                            type="module"
+                            src="https://unpkg.com/rapidoc@9.1.4/dist/rapidoc-min.js"
+                        ></script>
+                    </head>
+                    <body>
+                        <rapi-doc spec-url="{app.openapi_url}"></rapi-doc>
+                    </body>
+                </html>
+            """
+
+        if self.check_ptype == True:
+
+            @app.post("/predict/")
+            async def prediction(input_data: ptype):
+
+                served_data = _prepare_data(input_data)
+
+                y = served_model.predict([served_data])
+
+                return {"prediction": y[0]}
+
+        else:
+
+            @app.post("/predict/")
+            async def prediction(input_data):
+
+                input_data = input_data.split(" ")  # user delimiter ?
+                input_data = np.asarray(input_data)
+                reshape_data = input_data.reshape(1, -1)
+                y = served_model.predict(reshape_data)
+
+                return {"prediction": y[0]}
+
+        return app
+
+    def vetiver_post(self, endpoint_fx, endpoint_name):
+        if self.check_ptype == True:
+
+            @self.app.post("/" + endpoint_name + "/")
+            def custom_endpoint(input_data: self.model.ptype[0]):
+                served_data = _prepare_data(input_data)
+                new = endpoint_fx(pd.Series(served_data))
+                return new
+
+        else:
+
+            @self.app.post("/" + endpoint_name + "/")
+            def custom_endpoint(input_data):
+                served_data = _prepare_data(input_data)
+                new = endpoint_fx(served_data)
+                return new
 
     def run(self):
-        if _jupyter_nb() == True:
-            warnings.warn(
-                "WARNING: Jupyter Notebooks are not considered stable environments for production code")
-            nest_asyncio.apply()   
+        _jupyter_nb()
         uvicorn.run(self.app, port=self.port, host=self.host)
 
+    def predict(self, data: dict, endpoint):
+        response = requests.post(endpoint, json=data)
 
+        return response.json()
 
-def _init_app(vetiver_model, check_ptype):
-    model = _prepare_model(vetiver_model.model[0])
-    ptype = vetiver_model.ptype[0]
-
-    app = FastAPI()
-
-    @app.get("/", response_class=HTMLResponse, include_in_schema=False)
-    async def rapidoc():
-        return f"""
-            <!doctype html>
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                    <script
-                        type="module"
-                        src="https://unpkg.com/rapidoc@9.1.4/dist/rapidoc-min.js"
-                    ></script>
-                </head>
-                <body>
-                    <rapi-doc spec-url="{app.openapi_url}"></rapi-doc>
-                </body>
-            </html>
-        """
-
-    if check_ptype == True:
-        @app.post("/predict/")
-        async def prediction(input_data: ptype):
-
-            served_data = _prepare_data(input_data)
-
-            y = model.predict([served_data])
-
-            return {"prediction": y[0]}
-
-    else:
-        @app.post("/predict/")
-        async def prediction(input_data):
-
-            input_data = input_data.split(" ")  # user delimiter ?
-            input_data = np.asarray(input_data)
-            reshape_data = input_data.reshape(1, -1)
-            y = model.predict(reshape_data)
-
-            return {"prediction": y[0]}
-
-    ## add another endpoint?
-
-    return app
 
 def _prepare_model(model):
     joblib.dump(model, "vetiver_model.joblib")  # will eventually go
@@ -92,32 +120,13 @@ def _prepare_model(model):
     return load_model
 
 
-def _jupyter_nb():
-    try:
-        shell = get_ipython().__class__.__name__
-        if shell == "ZMQInteractiveShell":
-            return True  # Jupyter notebook
-        else:
-            return False
-    except NameError:
-        return False
-
-
 def _prepare_data(pred_data):
-
     served_data = []
     for key, value in pred_data:
         served_data.append(value)
-
     return served_data
 
 
 def vetiver_endpoint(url="http://127.0.0.1:8000/predict"):
 
     return url
-
-
-def vetiver_predict(data):
-
-    if isinstance(data, pd.DataFrame):
-        data.to_json(orient="records")
