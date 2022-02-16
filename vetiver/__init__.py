9a39dc9756c5e722edd7104183430f289d9b9448
isabelizimm <isabel.zimmerman@rstudio.com>
2022-02-16 13:03:46 -0500

    create automatic dummy data

--- vetiver/__init__.py
+++ vetiver/__init__.py
@@ -1,11 +1,12 @@
 """vetiver - Python parallel to R vetiver package"""
 
-__version__ = '0.1.0'
-__author__ = 'Isabel Zimmerman <isabel.zimmerman@rstudio.com>'
+__version__ = "0.1.0"
+__author__ = "Isabel Zimmerman <isabel.zimmerman@rstudio.com>"
 __all__ = []
 
 import pandas as pd
 import numpy as np
 from vetiver.ptype import *
 from vetiver.vetiver_model import *
-from vetiver.server import * 
+from vetiver.server import *
+from vetiver.dummy import *
