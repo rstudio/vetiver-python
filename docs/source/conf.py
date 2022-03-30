e62fe2ae524321ebc2b954ebe3c9627c9206a143
isabelizimm <isabel.zimmerman@rstudio.com>
2022-03-29 20:21:38 -0400

    typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ extensions = extensions = [
     "sphinx.ext.autosummary",
     "sphinx.ext.napoleon",
     "sphinx.ext.githubpages",
-    "sphinx.ext.autodoc" "myst_parser",
+    "sphinx.ext.autodoc",
 ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
