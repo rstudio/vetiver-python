7bb9e5c401864fd9a3bf9b32ea95ef784bd3d7d3
isabelizimm <isabel.zimmerman@rstudio.com>
2022-06-17 16:59:07 -0400

    add dependencies

--- vetiver/monitor.py
+++ vetiver/monitor.py
@@ -5,12 +5,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-def compute_metrics(data, 
-                    date_var, 
-                    period,
-                    metric_set, 
-                    truth, 
-                    estimate):
+
+def compute_metrics(data, date_var, period, metric_set, truth, estimate):
     """
     Compute metrics for given time period
 
@@ -18,13 +14,13 @@ def compute_metrics(data,
     ----------
     data : DataFrame
         Pandas dataframe
-    date_var: 
+    date_var:
         Column in `data` containing dates
     period:
-        Defining period to group by 
+        Defining period to group by
         https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases
     metric_set: list
-        List of metrics to compute 
+        List of metrics to compute
         https://scikit-learn.org/stable/modules/classes.html#module-sklearn.metrics
     truth:
         Column identifier for true results
@@ -33,57 +29,56 @@ def compute_metrics(data,
 
     """
     output = pd.DataFrame()
+    data = data[[date_var, truth, estimate]]
+
     dates_sorted = data.set_index(date_var).sort_index()
     rol = dates_sorted.set_index(truth).rolling(window=period)
 
     for metric in metric_set:
-        rol[metric.__name__] =rol.apply(
-            _compute, 
-            raw=False, 
-            kwargs = {
-            'metric': metric}
-            )
+        output[metric.__name__] = rol.apply(_compute, 
+                                        raw=False, 
+                                        kwargs={"metric": metric})
     
-    rol.set_index(date_var)
+    #BAD
+    dates_sorted = dates_sorted.reset_index()
+    output = pd.concat([dates_sorted[date_var], 
+                        output.reset_index().drop(columns=truth)], 
+                        axis = 1).set_index(date_var)
 
     return output
 
-def _compute(window, 
-            metric):
 
-    output = metric(y_true = window.index,
-                    y_pred=window)
+def _compute(window, metric):
+
+    output = metric(y_true=window.index, y_pred=window)
     return output
 
 
-def pin_metrics(board,
-                df_metrics,
-                metrics_pin_name,
-                overwrite = False):
+def pin_metrics(board, df_metrics, metrics_pin_name, overwrite=False):
     """
     Update an existing pin storing model metrics over time
 
     Parameters
     ----------
-    board : 
+    board :
         Pins board
     df_metrics: pd.DataFrame
         Dataframe of metrics over time, such as created by `vetiver_compute_metrics()`
     metrics_pin_name:
         Pin name for where the metrics are stored
     overwrite: bool
-        If TRUE (the default), overwrite any metrics for 
-        dates that exist both in the existing pin and 
-        new metrics with the new values. If FALSE, error 
-        when the new metrics contain overlapping dates with 
+        If TRUE (the default), overwrite any metrics for
+        dates that exist both in the existing pin and
+        new metrics with the new values. If FALSE, error
+        when the new metrics contain overlapping dates with
         the existing pin.
     """
     date_types = (datetime.date, datetime.time, datetime.datetime)
     if not isinstance(df_metrics.index, date_types):
-        try: 
-            df_metrics = df_metrics.index.astype('datetime')
+        try:
+            df_metrics = df_metrics.index.astype("datetime")
         except TypeError:
-            raise TypeError("Index must be a date type")
+            raise TypeError(f"Index of {df_metrics} must be a date type")
 
     new_metrics = df_metrics.sort_index()
 
@@ -100,7 +95,8 @@ def pin_metrics(board,
         old_metrics = old_metrics not in overlapping_dates
     else:
         if overlapping_dates:
-            raise ValueError(f"The new metrics overlap with dates \
+            raise ValueError(
+                f"The new metrics overlap with dates \
                      already stored in {repr(metrics_pin_name)} \
                      Check the aggregated dates or use `overwrite = True`"
             )
@@ -111,16 +107,12 @@ def pin_metrics(board,
     pins.pin_write(board, new_metrics, metrics_pin_name)
 
 
-def plot_metrics(df_metrics,
-                        date_var, metric):
+def plot_metrics(df_metrics, metric, date_var = None):
 
     ncols = 1
     nrows = len(metric)
 
-    fig, axes = plt.subplots(
-        nrows=nrows,
-        ncols=ncols
-    )
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols)
     """
     Plot metrics over a given time period
 
@@ -134,15 +126,18 @@ def plot_metrics(df_metrics,
         List of metrics to compute
 
     """
-
+    # date_types = (datetime.date, datetime.time, datetime.datetime)
+    
+    # if not isinstance(df_metrics.index, date_types):
+    #     try:
+    #         df_metrics = df_metrics.index.astype("datetime")
+    #     except:
+    #         raise TypeError("need datetime")
+    
     # loop for plotting each column
     for i, col in enumerate(df_metrics.columns):
         sns.lineplot(
-            x=date_var, 
-            y=df_metrics[col], 
-            ax=axes[i], 
-            color='royalblue'
-            )\
-            .set_title(col)
+            x=df_metrics.index, y=df_metrics[col], ax=axes[i], color="royalblue"
+        ).set_title(col)
 
     fig.tight_layout()
