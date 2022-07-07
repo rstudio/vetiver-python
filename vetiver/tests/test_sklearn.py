47b6c5b9d6bf945c0c54763db454ae508879d30f
isabelizimm <isabel.zimmerman@rstudio.com>
2022-07-07 09:46:01 -0400

    remove trailing slashes

--- vetiver/tests/test_sklearn.py
+++ vetiver/tests/test_sklearn.py
@@ -24,7 +24,7 @@ def test_predict_endpoint_ptype():
     np.random.seed(500)
     client = TestClient(_start_application().app)
     data = {"B": 0, "C": 0, "D": 0}
-    response = client.post("/predict/", json=data)
+    response = client.post("/predict", json=data)
     assert response.status_code == 200, response.text
     assert response.json() == {"prediction": [44.47]}, response.json()
 
@@ -33,7 +33,7 @@ def test_predict_endpoint_ptype_batch():
     np.random.seed(500)
     client = TestClient(_start_application().app)
     data = [{"B": 0, "C": 0, "D": 0}, {"B": 0, "C": 0, "D": 0}]
-    response = client.post("/predict/", json=data)
+    response = client.post("/predict", json=data)
     assert response.status_code == 200, response.text
     assert response.json() == {"prediction": [44.47, 44.47]}, response.json()
 
@@ -42,7 +42,7 @@ def test_predict_endpoint_ptype_error():
     np.random.seed(500)
     client = TestClient(_start_application().app)
     data = {"B": 0, "C": "a", "D": 0}
-    response = client.post("/predict/", json=data)
+    response = client.post("/predict", json=data)
     assert response.status_code == 422, response.text  # value is not a valid integer
 
 
@@ -50,7 +50,7 @@ def test_predict_endpoint_no_ptype():
     np.random.seed(500)
     client = TestClient(_start_application(save_ptype=False).app)
     data = "0,0,0"
-    response = client.post("/predict/", json=data)
+    response = client.post("/predict", json=data)
     assert response.status_code == 200, response.text
     assert response.json() == {"prediction": [44.47]}, response.json()
 
@@ -59,7 +59,7 @@ def test_predict_endpoint_no_ptype_batch():
     np.random.seed(500)
     client = TestClient(_start_application(save_ptype=False).app)
     data = [["0,0,0"], ["0,0,0"]]
-    response = client.post("/predict/", json=data)
+    response = client.post("/predict", json=data)
     assert response.status_code == 200, response.text
     assert response.json() == {"prediction": [44.47, 44.47]}, response.json()
 
@@ -69,4 +69,4 @@ def test_predict_endpoint_no_ptype_error():
     client = TestClient(_start_application(save_ptype=False).app)
     data = {"hell0", 9, 32.0}
     with pytest.raises(TypeError):
-        client.post("/predict/", json=data)
+        client.post("/predictt", json=data)
