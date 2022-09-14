bb4fe6025ed61f13e867b8373a155e0f744e9385
isabelizimm <isabel.zimmerman@rstudio.com>
2022-09-14 11:14:00 -0400

    updates from review

--- vetiver/model_card.py
+++ vetiver/model_card.py
@@ -9,6 +9,11 @@ def model_card(path="."):
     ----------
     path : str
        Path to save model card
+
+    Notes
+    -----
+    This model card is generated as a Quarto document. For more info on
+    Quarto, visit https://quarto.org/
     """
     src_path = _files("vetiver") / "templates/model_card.qmd"
 
