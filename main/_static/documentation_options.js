22159a9c889f03c2d306d9ad0211091cbb8601e2
isabelizimm <isabelizimm@users.noreply.github.com>
2022-09-21 14:38:51 +0000

    deploy: 6fc209d26acdaf72076ea0ff191892b9e26f2625

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Commit: 6a3c5e8',
+    VERSION: 'Commit: 6fc209d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
