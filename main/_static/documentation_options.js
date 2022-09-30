71782af1703e22eeab70c8e187078c30607e4730
isabelizimm <isabelizimm@users.noreply.github.com>
2022-09-30 14:01:48 +0000

    deploy: f8f863229cd96c50acc1daf35f534c66beed82c6

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Commit: fdf6485',
+    VERSION: 'Commit: f8f8632',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
