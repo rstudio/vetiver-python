982251eb43221d6800849848f86e9dae4f197e75
isabelizimm <isabel.zimmerman@rstudio.com>
2022-12-13 13:45:48 -0500

    refactoring docker test again

--- script/setup-docker/docker.py
+++ script/setup-docker/docker.py
@@ -12,19 +12,6 @@ board = pins.board_folder("pinsboard", allow_pickle_read=True)
 v = vetiver.VetiverModel(model, "mymodel", ptype_data=X)
 
 vetiver.vetiver_pin_write(board, v)
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 vetiver.prepare_docker(board, "mymodel")
-=======
-vetiver.load_pkgs(
-    v,
-    packages=["git+https://github.com/rstudio/vetiver-python@metadata"],
-    path="vetiver_",
-)
-=======
-vetiver.load_pkgs(v, path="vetiver_")
->>>>>>> aa70c3e (always install latest vetiver in docker)
-vetiver.write_app(board, "mymodel")
-vetiver.write_docker()
->>>>>>> 875faf2 (install into docker from branch)
