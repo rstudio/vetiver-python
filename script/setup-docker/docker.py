da0e9adaca82c49c8532c6f05ea06a4c878d41a6
isabelizimm <isabel.zimmerman@rstudio.com>
2022-12-13 13:41:44 -0500

    always install latest vetiver in docker

--- script/setup-docker/docker.py
+++ script/setup-docker/docker.py
@@ -13,6 +13,7 @@ v = vetiver.VetiverModel(model, "mymodel", ptype_data=X)
 
 vetiver.vetiver_pin_write(board, v)
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 vetiver.prepare_docker(board, "mymodel")
 =======
@@ -21,6 +22,9 @@ vetiver.load_pkgs(
     packages=["git+https://github.com/rstudio/vetiver-python@metadata"],
     path="vetiver_",
 )
+=======
+vetiver.load_pkgs(v, path="vetiver_")
+>>>>>>> aa70c3e (always install latest vetiver in docker)
 vetiver.write_app(board, "mymodel")
 vetiver.write_docker()
 >>>>>>> 875faf2 (install into docker from branch)
