f84f5b7a8dd40dc64fb93f0d133dd2a88e3208bc
isabelizimm <isabelizimm@users.noreply.github.com>
2023-01-18 23:11:25 +0000

    deploy: 84d03ff55ae21af7b558cd9b5903cf7cba8bb16e

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Commit: bcbab9c',
+    VERSION: 'Commit: 84d03ff',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
