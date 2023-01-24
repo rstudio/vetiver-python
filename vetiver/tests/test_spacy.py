9d3577add7fe1c539db27eafbd748ac6e1a3e082
isabelizimm <isabel.zimmerman@rstudio.com>
2023-01-24 10:50:25 -0500

    specify in server to not preprocess data

--- vetiver/tests/test_spacy.py
+++ vetiver/tests/test_spacy.py
@@ -11,7 +11,6 @@ import vetiver  # noqa
 
 @pytest.fixture
 def spacy_model():
-<<<<<<< HEAD
     @spacy.language.Language.component("animals")
     def animal_component_function(doc):
         matches = matcher(doc)  # noqa
@@ -34,18 +33,6 @@ def spacy_model():
 @pytest.fixture
 def vetiver_client(spacy_model):  # With check_prototype=True
     app = vetiver.VetiverAPI(spacy_model, check_prototype=True)
-=======
-
-    nlp = spacy.blank("en")
-    sentencizer = nlp.add_pipe("sentencizer")
-
-    return vetiver.VetiverModel(sentencizer, "sentencizer")
-
-
-@pytest.fixture
-def vetiver_client(spacy_model):  # With check_ptype=True
-    app = vetiver.VetiverAPI(spacy_model, check_ptype=True)
->>>>>>> b600e38 (testing spacy)
     app.app.root_path = "/predict"
     client = TestClient(app.app)
 
@@ -56,17 +43,19 @@ def vetiver_client(spacy_model):  # With check_ptype=True
 <<<<<<< HEAD
 def vetiver_client_check_ptype_false(spacy_model):  # With check_prototype=False
     app = vetiver.VetiverAPI(spacy_model, check_prototype=True)
+<<<<<<< HEAD
 =======
 def vetiver_client_check_ptype_false(spacy_model):  # With check_ptype=True
     app = vetiver.VetiverAPI(spacy_model, check_ptype=False)
 >>>>>>> b600e38 (testing spacy)
+=======
+>>>>>>> 64158ab (specify in server to not preprocess data)
     app.app.root_path = "/predict"
     client = TestClient(app.app)
 
     return client
 
 
-<<<<<<< HEAD
 def test_vetiver_post(vetiver_client):
     df = pd.DataFrame({"text": ["one", "my turtle is smarter than my dog"]})
 
@@ -107,55 +96,7 @@ def test_vetiver_post(vetiver_client):
 #     board = pins.board_temp(allow_pickle_read=True)
 #     vetiver.vetiver_pin_write(board=board, model=spacy_model)
 #     assert isinstance(
-#         board.pin_read("animals"),
-#         spacy.Language,
+#         board.pin_read("sentencizer"),
+#         spacy.pipeline.sentencizer.Sentencizer,
 #     )
-#     board.pin_delete("animals")
-=======
-def test_vetiver_build(vetiver_client):
-    nlp = spacy.blank("en")
-    words = ["This", "is", "a", "new", "Sentence", "."]
-    doc = spacy.tokens.Doc(nlp.vocab, words=words)
-
-    response = vetiver.predict(endpoint=vetiver_client, data=doc.to_json())
-
-    assert response == [[True, False, False, False, False, False]]
-
-
-def test_batch(vetiver_client):
-    nlp = spacy.blank("en")
-    words1 = ["This", "is", "a", "new", "Sentence", "."]
-    doc1 = spacy.tokens.Doc(nlp.vocab, words=words1)
-    words2 = ["Another", "one", "."]
-    doc2 = spacy.tokens.Doc(nlp.vocab, words=words2)
-
-    response = vetiver.predict(endpoint=vetiver_client, data=[doc1, doc2])
-
-    assert response == [[True, False, False, False, False, False], [True, False, False]]
-
-
-def test_no_ptype(vetiver_client_check_ptype_false):
-    nlp = spacy.blank("en")
-    words1 = ["This", "is", "a", "new", "Sentence", "."]
-    doc1 = spacy.tokens.Doc(nlp.vocab, words=words1)
-    words2 = ["Another", "one", "."]
-    doc2 = spacy.tokens.Doc(nlp.vocab, words=words2)
-
-    response = vetiver.predict(
-        endpoint=vetiver_client_check_ptype_false, data=[doc1, doc2]
-    )
-
-    assert response == [[True, False, False, False, False, False], [True, False, False]]
-
-
-def test_serialize(spacy_model):
-    import pins
-
-    board = pins.board_temp(allow_pickle_read=True)
-    vetiver.vetiver_pin_write(board=board, model=spacy_model)
-    assert isinstance(
-        board.pin_read("sentencizer"),
-        spacy.pipeline.sentencizer.Sentencizer,
-    )
-    board.pin_delete("sentencizer")
->>>>>>> b600e38 (testing spacy)
+#     board.pin_delete("sentencizer")
